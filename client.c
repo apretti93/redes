27d27a6dee05d7d2fb88dbb1d7bd412c94c053ec
apretti93 <apretti93@gmail.com>
2018-09-17 22:52:31 -0300

    Update client.c

--- client.c
+++ client.c
@@ -9,62 +9,58 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+int sizeBuff;
 
-int sizeBuffer;
-
-void error(int errorCode, const char* responseMessage)  {
-    printf("Error Code: %d - Response Message: %s\n", errorCode, responseMessage);
+void error( const char* errMsg)  {
+    printf("Error: %s\n", errMsg);
     exit(1);
 }
 
-void list(int uSocket) {
-    char uBuffer[sizeBuffer];
-
-    ssize_t bytesRec = recv(uSocket, uBuffer, sizeBuffer, 0);
+void list(int sock) {
+    char buffer[sizeBuff];
+    ssize_t bytesRec = recv(sock, buffer, sizeBuff, 0);
     while (bytesRec > 0) {
-        printf("%s\n", uBuffer);
-        bytesRec = recv(uSocket, uBuffer, sizeBuffer, 0);
+        printf("%s\n", buffer);
+        bytesRec = recv(sock, buffer, sizeBuff, 0);
       }
 }
 
-void get(int uSocket, const char *fileName) {
-    int uFile;
+void get(int sock, const char *fileName) {
+    int arq;
     ssize_t read_return;
-    if((uFile = open(fileName, O_CREAT|O_WRONLY, 0777)) == -1) {
-        error(-13, "Fail to create file");
+    if((arq = open(fileName, O_CREAT|O_WRONLY, 0777)) == -1) {
+        error("Error to create file");
     }
 
-    char uBuffer[sizeBuffer];
-
+    char buffer[sizeBuff];
     struct timeval t1, t2;
     double elapsedTime;
 
     gettimeofday(&t1, NULL);
-    ssize_t readBytes = read(uSocket, uBuffer, sizeBuffer);
+    ssize_t readBytes = read(sock, buffer, sizeBuff);
     while (readBytes > 0) {
-        if (write(uFile, uBuffer, sizeBuffer) == -1) {
-            error(-12, "Fail to R/W");
+        if (write(arq, buffer, sizeBuff) == -1) {
+            error("Error to R/W file");
         }
-        readBytes = read(uSocket, uBuffer, sizeBuffer);
+        readBytes = read(sock, buffer, sizeBuff);
     }
 
     if (readBytes < 0) {
-        error(-7, "Fail to connect client-server");
+        error( "Error linking client/server");
     }
 
-    close(uFile);
+    close(arq);
     gettimeofday(&t2, NULL);
 
-    //milliseconds convertion
-    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;   
     elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   
-    printf("Received (Total: %.2f milliseconds!\n", elapsedTime);
+    printf("Sucess! (Total time: %.2f ms!\n", elapsedTime);
 }
 
 int main(int argc, char *argv[]) {
 
     if (argc != 5 && argc != 6) {
-        error(-1, "Input error");
+        error( "Argc is not 5 or 6 - Error");
     }
 
     char *command = argv[1];
@@ -73,62 +69,61 @@ int main(int argc, char *argv[]) {
     in_port_t serverPort;
     char* message = (char *) malloc(sizeof(char) * 256);
 
-    if (strcmp(command, "list") == 0) {
-        server = argv[2];
-        serverPort = atoi(argv[3]);
-        sizeBuffer = atoi(argv[4]);
-    } else if (strcmp(command, "get") == 0) {
+
+    if (strcmp(command, "get") == 0) {
         fileName = argv[2];
         server = argv[3];
         serverPort = atoi(argv[4]);
-        sizeBuffer = atoi(argv[5]);
-    } else {
-        error(-10, "Client command not found");
+        sizeBuff = atoi(argv[5]);
+    }
+    else if (strcmp(command, "list") == 0) {
+        server = argv[2];
+        serverPort = atoi(argv[3]);
+        sizeBuff = atoi(argv[4]);
+    }  else {
+        error( "Client command not exists");
     }
 
-    int uSocket = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+    int sock = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
 
-    if (uSocket < 0){
-        error(-2, "Fail to create socket");
+    if (sock < 0){
+        error( "Error to create socket");
     }
 
-    struct sockaddr_in6 serverAddress; 
+    struct sockaddr_in6 servAddr;
 
-    memset(&serverAddress, 0, sizeof(serverAddress)); 
-    serverAddress.sin6_family = AF_INET6;
-
-    int rtnVal = inet_pton(AF_INET6, server, &serverAddress.sin6_addr.s6_addr);
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin6_family = AF_INET6;
+    int rtnVal = inet_pton(AF_INET6, server, &servAddr.sin6_addr.s6_addr); //conversão do endereço de txt pra binary
 
     if (rtnVal < 0) {
-        error(-101, "Fail to Address convertion");
+        error( "Fail to convert address");
     }
     else if (rtnVal == 0) {
-        error(-100, "invalid Address");
+        error( "Invalid Address");
     }
 
-    serverAddress.sin6_port = htons(serverPort); 
-
-    if (connect(uSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
-        error(-6, "Connection Error");
+    servAddr.sin6_port = htons(serverPort); //porta do servidor
+    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0){  // conectando ao servidor
+        error( "Erro de connect");
     }
 
     sprintf(message, "%s %s", command, fileName);
     size_t messageLen = strlen(message);
+    ssize_t numBytes = send(sock, message, messageLen, 0);
 
-    ssize_t numBytes = send(uSocket, message, messageLen, 0);
-
-    if (numBytes >= 0)
-        printf("send() - Sending message\n");
+    if (numBytes < 1 && numBytes != 0)
+        error( "Error linking client/server\n");
     else{
-        error(-7, "Fail to connect client-server\n");
+        printf("Sending message to server...\n");
+    }	
+
+    if (strcmp(command, "list") == 0) {
+        list(sock);
+    } else if (strcmp(command, "get") == 0) {
+        get(sock, fileName);
     }
 
-    if (strcmp(command, "get") == 0) {
-        get(uSocket, fileName);
-    } else if (strcmp(command, "list") == 0) {
-        list(uSocket);
-    }
-
-    close(uSocket);
+    close(sock);
     exit(0);
 }
